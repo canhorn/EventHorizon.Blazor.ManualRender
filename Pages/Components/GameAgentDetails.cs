29525deaac139a8068c1ffceca9e3a97f0647e60
Cody Merritt Anhorn <cody.anhorn@hotmail.com>
2020-04-26 20:05:59 -0500

    Initial Commit.

---
+++ Pages/Components/GameAgentDetails.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace EventHorizon.Blazor.ManualRender.Pages.Components
+{
+    /// This Component is completely generated in C#, without the use of any .razor file.
+    public class GameAgentDetails : ComponentBase
+    {
+        [Parameter]
+        public string Name { get; set; }
+        [Parameter]
+        public string Role { get; set; }
+        [Parameter]
+        public string Function { get; set; }
+
+        // This is the BuildRenderTree, we can use this to build from scratch the HTML that will be placed on the page.
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            var seq = 0;
+            // We open a div
+            builder.OpenElement(seq, "div");
+            builder.AddAttribute(++seq, "class", "agent-details__name");
+            // Then add some conetnt to that the div, in this case the passed in Name parameter.
+            builder.AddContent(++seq, Name);
+            // We then close the currently open element.
+            builder.CloseElement();
+            
+            builder.OpenElement(++seq, "div");
+            builder.AddAttribute(++seq, "class", "agent-details__role");
+            builder.AddContent(++seq, Role);
+            builder.CloseElement();
+
+            builder.OpenElement(++seq, "div");
+            builder.AddAttribute(++seq, "class", "agent-details__function");
+            builder.AddContent(++seq, Function);
+            builder.CloseElement();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# About
+
+This project is an example of how you can, in ASP.NET Blazor, create a component without using a razor file.
+
+# Checkout the Blog Post for this Project
+[[BLOG POST URL HERE]]
